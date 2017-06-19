59243d11663d26639d45a0e3f2d5b125f399254a
Yixiu Jiang <yixiu.jiang@gmail.com>
2017-06-19 23:08:11 +1000

    implement sos

--- MorseTorch/ViewController.swift
+++ MorseTorch/ViewController.swift
@@ -32,16 +32,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func sosFlash(_ sender: Any) {
-        dotFlash()
-        dotFlash()
-        dotFlash()
-        dashFlash()
-        dashFlash()
-        
-        dashFlash()
-        dotFlash()
-        dotFlash()
-        dotFlash()
+        start()
         
     }
     func toggleFlash() {
@@ -70,7 +61,7 @@ class ViewController: UIViewController {
     
     func dashFlash(){
         let delay = Int(1 * Double(1000))
-
+        
         flash(flashSeconds: DispatchTime.now() + .milliseconds(delay))
         
     }
@@ -91,13 +82,107 @@ class ViewController: UIViewController {
                     print(error)
                 }
                 let sleep = DispatchTime.now() + .milliseconds(Int(1 * Double(1000)))
-
+                
                 DispatchQueue.main.asyncAfter(deadline:sleep) {}
             }
             torchDevice.unlockForConfiguration()
         } catch {
             print(error)
         }
+    }
+    
+    /// Short signal duration (LED on)
+    private static let shortInterval = 0.2
+    /// Long signal duration (LED on)
+    private static let longInterval = 0.4
+    /// Pause between signals (LED off)
+    private static let pauseInterval = 0.2
+    /// Pause between the whole SOS sequences (LED off)
+    private static let sequencePauseInterval = 2.0
+    
+    /**
+     When the SOS sequence is started flashlight is on. Thus
+     the first time interval is for the short signal. Then pause,
+     then short, ...
+     
+     See `timerTick()`, it alternates flashlight status (on/off) based
+     on the current index in this sequence.
+     */
+    private let sequenceIntervals = [
+        shortInterval, pauseInterval, shortInterval, pauseInterval, shortInterval, pauseInterval,
+        longInterval, pauseInterval, longInterval, pauseInterval, longInterval, pauseInterval,
+        shortInterval, pauseInterval, shortInterval, pauseInterval, shortInterval, sequencePauseInterval
+    ]
+    
+    /// Current index in the SOS `sequence`
+    private var index: Int = 0
+    
+    /// Non repeatable timer, because time interval varies
+    private weak var timer: Timer?
+    
+    /**
+     Put your `Flashlight()` calls inside this function.
+     
+     - parameter on: pass `true` to turn it on or `false` to turn it off
+     */
+    private func turnFlashlight(on: Bool) {
+        // if on == true  -> turn it on
+        // if on == false -> turn it off
+        if (on == true){
+            if (torchDevice.hasTorch) {
+                do {
+                    try torchDevice.lockForConfiguration()
+                    torchDevice.torchMode = AVCaptureTorchMode.on
+                    torchDevice.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+                
+            }
+        }else{
+            if (torchDevice.hasTorch) {
+                do {
+                    try torchDevice.lockForConfiguration()
+                    torchDevice.torchMode = AVCaptureTorchMode.off
+                    torchDevice.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+                
+            }
+        }
+    }
+    
+    private func scheduleTimer() {
+        timer = Timer.scheduledTimer(timeInterval: sequenceIntervals[index], target: self, selector: #selector(ViewController.timerTick), userInfo: nil, repeats: false)
+    }
+    
+    @objc private func timerTick() {
+        index = index + 1
+        // Increase sequence index, at the end?
+        if index == sequenceIntervals.count {
+            // Start from the beginning
+            index = 0
+        }
+        // Alternate flashlight status based on current index
+        // index % 2 == 0 -> is index even number? 0, 2, 4, 6, ...
+        turnFlashlight(on: index % 2 == 0)
+        scheduleTimer()
+    }
+    
+    func start() {
+        index = 0
+        turnFlashlight(on: true)
+        scheduleTimer()
+    }
+    
+    func stop() {
+        timer?.invalidate()
+        turnFlashlight(on: false)
+    }
+    
+    deinit {
+        timer?.invalidate()
     }
 }
 
